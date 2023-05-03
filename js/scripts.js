0e9684db829cf98f04b88cb45e3fe144daffe8c6
franndev42 <130412811+franndev42@users.noreply.github.com>
2023-05-03 19:29:11 -0300

    Trabajo-N3

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+//inicialized AOS library
+AOS.init();
+
+
+let menu_bar = document.getElementById('menu_bar');
+let menu = document.getElementById('menu');
+
+menu_bar.addEventListener('click' , function (){
+    menu.classList.toggle('show-menu')
+} )
